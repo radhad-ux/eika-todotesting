ffe0720a3d6d6cd86e2f22384d141db52b909bf3
radhad-ux <radhad83@gmail.com>
2022-04-05 21:50:44 +0200

    test cases created on displaying screens

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Expected to go to the Welcome screen if the list is empty", () => {
+  //Arrange
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  //Act
+  const titleElement = screen.queryByText(/welcome to eika!/i);
+
+  //Assert
+  expect(titleElement).toBeInTheDocument();
+});
+
+test("Expected to go to the Shopping screen if the list is not empty", () => {
+  //Arrange
+  const fakeItem = {
+    id: 0,
+    name: "Glass",
+    price: 50,
+    isCompleted: false,
+  };
+
+  const fakeList = [fakeItem];
+  const fakeData = JSON.stringify(fakeList);
+
+  Storage.prototype.getItem = jest.fn(() => fakeData);
+  render(<App />);
+
+  //Act
+  const titleElement = screen.queryByText(/your shopping list/i);
+  screen.debug();
+  //Assert
+  expect(titleElement).toBeInTheDocument();
 });
