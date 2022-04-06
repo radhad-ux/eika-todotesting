deb13bafa87806b8a2e6addf923c963b4e86ce3b
radhad-ux <radhad83@gmail.com>
2022-04-06 11:26:53 +0200

    tested input field for correct and incorrect settings and props

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,4 +1,8 @@
 export default function InputField({ settings, state }) {
+  // safeguard
+//   if (settings === undefined) throw new Error("The setting prop is missing");
+//   if (state === undefined) throw new Error("The state prop is missing");
+
   const { label, type, placeholder, required, autoFocus } = settings;
   const [getter, setter] = state;
 
