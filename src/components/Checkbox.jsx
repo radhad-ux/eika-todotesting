9cbeae70e3b182af54276ef6ead9265d1c01320c
radhad-ux <radhad83@gmail.com>
2022-04-07 20:31:45 +0200

    testcase added to fire a event for checkbox

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -2,7 +2,12 @@
 export default function InputCheckbox({ checked, onChange }) {
   return (
     <label className="input_checkbox">
-      <input type="checkbox" checked={checked} onChange={onChange} />
+      <input
+        type="checkbox"
+        data-testid="checkbox"
+        checked={checked}
+        onChange={onChange}
+      />
       <div className={`icon_checkmark ${checked && "checked"}`} />
     </label>
   );
--- src/components/Checkbox.test.jsx
+++ src/components/Checkbox.test.jsx
@@ -1,1 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+import CheckBox from "./Checkbox";
+
+test("Expected to toggle checkbox when clicked", () => {
+  const toggleMethod = jest.fn();
+  render(<CheckBox checked={true} onChange={toggleMethod} />);
+
+  const checkBox = screen.getByTestId("checkbox");
+
+  expect(checkBox).toBeInTheDocument();
+  fireEvent.click(checkBox);
+  expect(toggleMethod).toHaveBeenCalledTimes(1);
+});
